1c256d09b1145db771938c6a0802a4466bdd52f5
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-09-23 19:59:39 +0800

    MyBatisGenerator

---
+++ src/test/java/io/zbc/motorecord/MyBatisGeneratorTest.java
@@ -0,0 +1,29 @@
+package io.zbc.motorecord;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyBatisGeneratorTest {
+    public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream configFile = classloader.getResourceAsStream("mybatis-generator-config.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
