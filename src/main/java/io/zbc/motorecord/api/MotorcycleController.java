9f98d3233285204814fdfb722000eca1c83e5087
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-09-27 19:05:49 +0800

    Update MotorcycleController.java

--- src/main/java/io/zbc/motorecord/api/MotorcycleController.java
+++ src/main/java/io/zbc/motorecord/api/MotorcycleController.java
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/motorcycle")
 public class MotorcycleController {
 
+    @RequestMapping(value = "/findMyMotorcycle")
+    public String findMyMotorcycle(@RequestBody String userId) {
+
+        return null;
+    }
+
     @RequestMapping(value = "/createMyMotorcycle")
     public String createMyMotorcycle(@RequestBody Motorcycle motorcycle) {
 
         return null;
     }
-
 }
