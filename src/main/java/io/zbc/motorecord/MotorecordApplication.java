4153c355bfed6b9e8613cc173e89473fbbee6287
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-09-28 17:03:57 +0800

    添加Spring和MyBatis扫描bean目录

--- src/main/java/io/zbc/motorecord/MotorecordApplication.java
+++ src/main/java/io/zbc/motorecord/MotorecordApplication.java
@@ -1,9 +1,13 @@
 package io.zbc.motorecord;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"io.zbc.motorecord"})
+@MapperScan({"io.zbc.motorecord.mapper"})
 public class MotorecordApplication {
 
     public static void main(String[] args) {
